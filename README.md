9b0274de51db02c900ff4f672a6059ba4d5d8d30
Ted Romanus <tedromanus@Teds-MacBook-Pro.local>
2017-12-16 00:09:49 +0200

    refactor code; fix small bugs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# license-processing
+# License Processing
+
+## Usage:
+
+#### Insert license to all files in Assets/Scripts folder except meta files:
+`node license -insert -src ./license.txt -path ./Assets/Scripts -exclude ".*\.meta"`
+
+#### Delete license from all files in Assets/Scripts folder except meta files:
+`node license -delete -path ./Assets/Scripts`
+
+#### Replace current license with new:
+`node license -replace -src ./license.txt -path ./Assets/Scripts -exclude ".*\.meta"`
---
+++ license.js
@@ -0,0 +1,174 @@
+/* 
+ * See all commands:
+ * node license -help
+ */
+
+const fs = require('fs');
+
+String.prototype.matches = function(regexp) {
+    return this.match(regexp) != null;
+};
+const readFile = (path, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.readFile(path, opts, (err, data) => {
+            if (err) rej(err)
+            else res(data)
+        })
+    });
+
+const writeFile = (path, data, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.writeFile(path, data, opts, (err) => {
+            if (err) rej(err)
+            else res()
+        })
+    });
+
+const getAllFilesFromPath = function(dir, filelist) {
+    var path = path || require('path');
+    var fs = fs || require('fs'),
+        files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+        filelist = getAllFilesFromPath(path.join(dir, file), filelist);
+        }
+        else {
+        filelist.push((dir.startsWith('./') ? dir : './' + dir) + "/" + file);
+        }
+    });
+    return filelist;
+};
+
+const insert = async (args) => {
+    let license_path = null;
+    let files_directory = null;
+    let exclude = /^(?!.*)/; // regexp that matches nothing
+
+    // get input data
+    args.forEach(function (val, index, array) {
+        switch(val){
+            case '-src': license_path = args[index+1]; break;
+            case '-path': files_directory = args[index+1]; break;
+            case '-exclude': exclude = new RegExp(args[index+1]); break;
+        }
+    });
+
+    // error checking
+    if(license_path == null){
+        console.error('Please enter license file path with -src flag');
+        return;
+    }
+    if(files_directory == null){
+        console.error('Please enter files directory with -path flag');
+        return;
+    }
+
+    // get license text
+    const license_text = await readFile(license_path);
+
+    // get all files from path
+    const files_list = getAllFilesFromPath(files_directory);
+    const filtered_files_list = files_list.filter(file_path => !file_path.matches(exclude));
+    console.log(`Adding licenses to ${filtered_files_list.length} files...`);
+
+    for(let file_path of filtered_files_list){
+        const file_content = await readFile(file_path);
+        const new_file_content = license_text + "\n\n" + file_content;
+        new_file_content.replace('\uFEFF', ''); // delete this annoying symbol
+        await writeFile(file_path, new_file_content);
+    }
+};
+
+const del = async (args) => {
+    const license_regexp = /^\/\*(.|\n|\r)*?\*\/(\n|\r|\s)*/;
+    let files_directory = null;
+    let exclude = /^(?!.*)/; // regexp that matches nothing
+
+    // get input data
+    args.forEach(function (val, index, array) {
+        switch(val){
+            case '-path': files_directory = args[index+1]; break;
+            case '-exclude': exclude = new RegExp(args[index+1]); break;
+        }
+    });
+
+    // error checking
+    if(files_directory == null){
+        console.error('Please enter files directory with -path flag');
+        return;
+    }
+
+    // get all files from path
+    const files_list = getAllFilesFromPath(files_directory);
+    const filtered_files_list = files_list.filter(file_path => !file_path.matches(exclude));
+    console.log(`Removing licenses from ${filtered_files_list.length} files...`);
+
+    for(let file_path of filtered_files_list){
+        let file_content = await readFile(file_path);
+        const license_regexp_result = file_content.match(license_regexp);
+        if(license_regexp_result == null || license_regexp_result.index != 0)
+            continue;
+        const cleaned_file = file_content.substring(license_regexp_result[0].length);
+        await writeFile(file_path, cleaned_file);
+    }
+};
+
+const replace = async (args) => {
+    await del(args);
+    await insert(args);
+};
+
+const help = () => {
+
+    // WARNING: if copy commands from next string, mind escaped backslashes!
+    console.log(`
+    Usage:
+
+    Insert license to all files in Assets/Scripts folder except meta files:
+    node license -insert -src ./license.txt -path ./Assets/Scripts -exclude ".*\\.meta"
+
+    Delete license from all files in Assets/Scripts folder except meta files:
+    node license -delete -path ./Assets/Scripts
+
+    Replace current license with new:
+    node license -replace -src ./license.txt -path ./Assets/Scripts -exclude ".*\\.meta"
+    `);
+};
+
+(async ()=>{
+    'use strict';
+
+    try{
+        const argv = process.argv;
+
+        if(argv.includes('-insert')){
+            await insert(argv);
+            console.log('Done.');
+            return;
+        }
+
+        if(argv.includes('-delete')){
+            await del(argv);
+            console.log('Done.');
+            return;
+        }
+
+        if(argv.includes('-replace')){
+            await replace(argv);
+            console.log('Done.');
+            return;
+        }
+
+        if(argv.includes('-help')){
+            help();
+            return;
+        }
+
+        console.log(`command '${argv[2]}' not found`);
+    }
+    catch(ex){
+        console.log(ex);
+    }
+
+})();
